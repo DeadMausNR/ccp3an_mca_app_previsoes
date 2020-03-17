7bc12c2a4108dd6272d1fb252e8476b7ae23ac3f
Nathan Lima <nathanlima123@gmail.com>
2020-03-17 12:54:48 -0300

    Redirecionamento padrão e tratamento de erro.

--- src/main/java/br/usjt/app_previsoes/controller/PrevisoesController.java
+++ src/main/java/br/usjt/app_previsoes/controller/PrevisoesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.usjt.app_previsoes.model.Previsao;
 
@@ -16,6 +17,11 @@ public class PrevisoesController {
 
 	@Autowired
 	private PrevisoesRepository previsoesRepo;
+	
+	@RequestMapping("/")
+	public String index() {
+		return "redirect:/Previsoes";
+	}
 
 	@GetMapping("/Previsoes")
 	public ModelAndView listarPrevisoes() {
